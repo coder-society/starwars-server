ee5f6be839a4f20604b724b3c1e6ab1b844eb91f
Kentaro Wakayama <kentaro@codersociety.com>
2020-11-05 11:19:33 +0100

    Fix subscriptions

--- data/swapiSchema.js
+++ data/swapiSchema.js
@@ -443,7 +443,7 @@ const resolvers = {
             () => pubsub.asyncIterator(ADDED_REVIEW_TOPIC),
             (payload, variables) => {
                 return (payload !== undefined) && 
-                ((variables.episode === null) || (payload.reviewAdded.episode === variables.episode));
+                ((variables.episode == null) || (payload.reviewAdded.episode === variables.episode));
             }
         ),
     },
